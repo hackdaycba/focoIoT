b6d228438601e99d0be43c1a31a152215870639b
malopez <malopez@vates>
2019-03-08 19:43:42 -0300

    first commit

--- dashboard/js/settings.js
+++ dashboard/js/settings.js
@@ -1,8 +1,8 @@
 var settings = {
-                host: "m16.cloudmqtt.com",
-                port: 35284,
-                user: "abjpjrxf",
-                pass: "rs-f855xX1vL",
-                client_id: "dash",
+                host: "localhost",
+                port: 1883,
+                user: "USER",
+                pass: "PASS",
+                client_id: "dashboard",
                 topic: "/foco"
             }
